bc802a26fece37284b92a09e21ea38a3b1e9a965
dmitchsplunk <derekmitchell@splunk.com>
2024-10-23 11:52:19 -0700

    added python lambda example

---
+++ instrumentation/python/aws-lambda/hello_world/app.py
@@ -0,0 +1,26 @@
+import json
+import requests
+import logging
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+def lambda_handler(event, context):
+
+    logger.info('In lambda_handler, about to get the IP address...')
+
+    try:
+        ip = requests.get("http://checkip.amazonaws.com/")
+    except requests.RequestException as e:
+        logger.error(e)
+        raise e
+
+    logger.info('Successfully got the IP address, returning a response.')
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+             "location": ip.text.replace("\n", "")
+        }),
+    }
