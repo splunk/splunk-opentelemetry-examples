9fac751e1c2ff70c23959b52e3de19298d974644
dmitchsplunk <derekmitchell@splunk.com>
2024-11-14 16:47:29 -0700

    added log context to node.js linux example

--- instrumentation/nodejs/linux/index.js
+++ instrumentation/nodejs/linux/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
+const logger = require('pino')()
 
 const PORT = process.env.PORT || "8080";
 
 app.get("/hello", (_, res) => {
+  logger.info('/hello endpoint invoked, sending response');
   res.status(200).send("Hello, World!");
 });
 
 app.listen(parseInt(PORT, 10), () => {
-  console.log(`Listening for requests on http://localhost:${PORT}`);
+  logger.info(`Listening for requests on http://localhost:${PORT}`);
 });
